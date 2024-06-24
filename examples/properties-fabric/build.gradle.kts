8863d5c43fde87c5dfa730cf9ced1d29d9c0220b
Cyan Kneelawk <kneelawk@gmail.com>
2024-06-23 17:12:35 -0700

    Add mod-menu

--- examples/properties-fabric/build.gradle.kts
+++ examples/properties-fabric/build.gradle.kts
@@ -10,3 +10,12 @@ submodule {
     applyXplatConnection(":properties-xplat", "fabric")
     generateRuns()
 }
+
+dependencies {
+    // Mod Menu
+    val mod_menu_version: String by project
+    modLocalRuntime("com.terraformersmc:modmenu:$mod_menu_version") {
+        exclude(group = "net.fabricmc")
+        exclude(group = "net.fabricmc.fabric-api")
+    }
+}
